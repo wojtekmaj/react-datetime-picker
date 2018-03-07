5dde65e5c85684c3b0b5c74f0e410f37785ba757
Wojciech Maj <kontakt@wojtekmaj.pl>
2018-03-08 00:49:56 +0100

    Add tests for dateFormatter

---
+++ src/shared/__tests__/dateFormatter.js
@@ -0,0 +1,24 @@
+import {
+  formatDate,
+  formatTime,
+} from '../dateFormatter';
+
+describe('formatDate', () => {
+  it('returns proper full numeric date', () => {
+    const date = new Date(2017, 1, 1);
+
+    const formattedDate = formatDate(date, 'en-US');
+
+    expect(formattedDate).toBe('2/1/2017');
+  });
+});
+
+describe('formatTime', () => {
+  it('returns proper full time', () => {
+    const date = new Date(2017, 1, 1, 13, 27);
+
+    const formattedTime = formatTime(date, 'de-DE');
+
+    expect(formattedTime).toBe('13:27:00');
+  });
+});
