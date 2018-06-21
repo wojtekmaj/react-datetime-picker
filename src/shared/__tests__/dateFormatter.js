b6d13c0eaae6eea2f16175dea61a31eb13e09ef3
Wojciech Maj <kontakt@wojtekmaj.pl>
2018-06-21 11:55:01 +0200

    Add tests for 24-hour format, skip locale-dependent tests if ICU is not available

--- src/shared/__tests__/dateFormatter.js
+++ src/shared/__tests__/dateFormatter.js
@@ -3,6 +3,18 @@ import {
   formatTime,
 } from '../dateFormatter';
 
+const hasFullICU = (() => {
+  try {
+    const date = new Date(2018, 0, 1, 21);
+    const formatter = new Intl.DateTimeFormat('de-DE', { hour: 'numeric' });
+    return formatter.format(date) === '21';
+  } catch (err) {
+    return false;
+  }
+})();
+
+const itIfFullICU = hasFullICU ? it : it.skip;
+
 describe('formatDate', () => {
   it('returns proper full numeric date', () => {
     const date = new Date(2017, 1, 1);
@@ -14,11 +26,19 @@ describe('formatDate', () => {
 });
 
 describe('formatTime', () => {
-  it('returns proper full time', () => {
+  it('returns proper full time (12-hour format)', () => {
     const date = new Date(2017, 1, 1, 13, 27);
 
     const formattedTime = formatTime(date, 'en-US');
 
     expect(formattedTime).toBe('1:27:00 PM');
+  });
+
+  itIfFullICU('returns proper full time (24-hour format)', () => {
+    const date = new Date(2017, 1, 1, 13, 27);
+
+    const formattedTime = formatTime(date, 'de-DE');
+
+    expect(formattedTime).toBe('13:27:00');
   });
 });
