b0cdc2af51c0f4a36bbc8083fb280be0bf11f288
Wojciech Maj <kontakt@wojtekmaj.pl>
2018-10-07 11:49:45 +0200

    Add support for event props

--- src/DateTimePicker.jsx
+++ src/DateTimePicker.jsx
@@ -1,6 +1,7 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { polyfill } from 'react-lifecycles-compat';
+import makeEventProps from 'make-event-props';
 import mergeClassNames from 'merge-class-names';
 import detectElementOverflow from 'detect-element-overflow';
 
@@ -31,6 +32,10 @@ export default class DateTimePicker extends PureComponent {
   }
 
   state = {};
+
+  get eventProps() {
+    return makeEventProps(this.props);
+  }
 
   componentDidMount() {
     document.addEventListener('mousedown', this.onClick);
@@ -81,7 +86,11 @@ export default class DateTimePicker extends PureComponent {
   }
 
   onFocus = (event) => {
-    const { disabled } = this.props;
+    const { disabled, onFocus } = this.props;
+
+    if (onFocus) {
+      onFocus(event);
+    }
 
     // Internet Explorer still fires onFocus on disabled elements
     if (disabled) {
@@ -314,6 +323,7 @@ export default class DateTimePicker extends PureComponent {
           `${baseClassName}--${disabled ? 'disabled' : 'enabled'}`,
           className,
         )}
+        {...this.eventProps}
         onFocus={this.onFocus}
         ref={(ref) => { this.wrapper = ref; }}
       >
