74f5c1475d5cd92a523d39271aa6b65454bf21f6
Wojciech Maj <kontakt@wojtekmaj.pl>
2019-02-02 16:21:59 +0100

    Fix propTypes

--- test/ValueOptions.jsx
+++ test/ValueOptions.jsx
@@ -59,6 +59,9 @@ ValueOptions.propTypes = {
   value: PropTypes.oneOfType([
     PropTypes.string,
     PropTypes.instanceOf(Date),
-    PropTypes.arrayOf(PropTypes.instanceOf(Date)),
+    PropTypes.arrayOf(PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.instanceOf(Date),
+    ])),
   ]),
 };
