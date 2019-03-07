13eb13878883b513f58ec292129717f83e849184
Idaho Edokpayi <idaho_edokpayi@ios.doi.gov>
2019-03-07 14:25:45 -0500

    whoops missed some changes

--- index.d.ts
+++ index.d.ts
@@ -2,38 +2,26 @@
 // Project: react-datetime-picker
 // Definitions by: Idaho Edokpayi https://www.npmjs.com/~doiedokpayi
 import * as React from "react";
+import { DatePickerProps } from "react-date-picker";
 
 export = DateTimePicker;
 
-declare class DateTimePicker extends React.Component<DateTimePicker.IDateTimePickerProps, DateTimePicker.IDateTimePickerState> {
-    public props: DateTimePicker.IDateTimePickerProps;    
-    constructor(props: DateTimePicker.IDateTimePickerProps);        
-    render(): React.ReactNode;
+interface DateTimePickerProps extends DatePickerProps {
+  clockClassName?: string | string[];
+  disableClock?: boolean;
+  isCalendarOpen?: boolean;
+  isClockOpen?: boolean;
+}
+interface IDateTimePickerState {
+  isCalendarOpen: boolean;
+  isCalendarOpenProps: boolean;
 }
 
-declare namespace DateTimePicker{
-    export interface IDateTimePickerProps{
-        calendarClassName?: string|string[];
-        clockClassName?: string|string[];
-        calendarIcon?: string| JSX.Element;
-        className?: string|string[];
-        clearIcon?: string|JSX.Element;
-        disabled?: boolean;
-        disableClock?: boolean;
-        isCalendarOpen?: boolean;
-        isClockOpen?: boolean;
-        locale?: string;
-        maxDate?: Date;
-        maxDetail?: string;
-        minDate?: Date;
-        minDetail?: string;
-        name?: string;
-        onChange: (value: Date) => void;
-        returnValue?: string;
-        required?: boolean;
-        showLeadingZeros?: boolean;
-        value: Date|Date[];}
-    export interface IDateTimePickerState{
-        isCalendarOpen: boolean;
-        isCalendarOpenProps: boolean;}
+declare class DateTimePicker extends React.Component<
+  DateTimePickerProps,
+  IDateTimePickerState
+> {
+  public props: DateTimePickerProps;
+  constructor(props: DateTimePickerProps);
+  render(): JSX.Element;
 }
