132b1895ec3324a214a0a3e115f811a8d924f8d3
Wojciech Maj <kontakt@wojtekmaj.pl>
2019-07-12 02:41:46 +1000

    Parse integers before passing them further

--- src/DateTimeInput.jsx
+++ src/DateTimeInput.jsx
@@ -519,15 +519,14 @@ export default class DateTimeInput extends PureComponent {
     } else if (
       formElements.every(formElement => formElement.value && formElement.checkValidity())
     ) {
-      const hour = values.hour24 || convert12to24(values.hour12, values.amPm);
-      const proposedValue = new Date(
-        values.year,
-        (values.month || 1) - 1,
-        values.day || 1,
-        hour,
-        values.minute || 0,
-        values.second || 0,
-      );
+      const year = parseInt(values.year, 10);
+      const month = parseInt(values.month || 1, 10);
+      const day = parseInt(values.day || 1, 10);
+      const hour = parseInt(values.hour24 || convert12to24(values.hour12, values.amPm) || 0, 10);
+      const minute = parseInt(values.minute || 0, 10);
+      const second = parseInt(values.second || 0, 10);
+
+      const proposedValue = new Date(year, month - 1, day, hour, minute, second);
       const processedValue = proposedValue;
       onChange(processedValue, false);
     }
