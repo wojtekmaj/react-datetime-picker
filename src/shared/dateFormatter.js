fbab6a6727789c267c719b54e831cdefbf811f76
Wojciech Maj <kontakt@wojtekmaj.pl>
2019-07-28 13:03:59 +0200

    Optimize Babel output

--- src/shared/dateFormatter.js
+++ src/shared/dateFormatter.js
@@ -2,9 +2,10 @@ import getUserLocale from 'get-user-locale';
 
 /* eslint-disable import/prefer-default-export */
 
-export const getFormatter = options => (locale, date) => (
-  date.toLocaleString(locale || getUserLocale(), options)
-);
+export function getFormatter(options) {
+  return (locale, date) => date.toLocaleString(locale || getUserLocale(), options);
+}
 
 const formatDateOptions = { day: 'numeric', month: 'numeric', year: 'numeric' };
+
 export const formatDate = getFormatter(formatDateOptions);
