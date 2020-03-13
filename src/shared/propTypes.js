3427541b708dddb52becbc7b1d5766bb1c729091
Wojciech Maj <kontakt@wojtekmaj.pl>
2020-03-13 13:26:57 +0100

    Reduce dependency on external libraries

--- src/shared/propTypes.js
+++ src/shared/propTypes.js
@@ -1,8 +1,44 @@
-export {
-  isMaxDate,
-  isMinDate,
-} from 'react-calendar/dist/shared/propTypes';
+import PropTypes from 'prop-types';
 
-export {
-  isValueType,
-} from 'react-time-picker/dist/shared/propTypes';
+const allViews = ['hour', 'minute', 'second'];
+const allValueTypes = [...allViews];
+
+export const isMinDate = (props, propName, componentName) => {
+  const { [propName]: minDate } = props;
+
+  if (minDate) {
+    if (!(minDate instanceof Date)) {
+      return new Error(`Invalid prop \`${propName}\` of type \`${typeof minDate}\` supplied to \`${componentName}\`, expected instance of \`Date\`.`);
+    }
+
+    const { maxDate } = props;
+
+    if (maxDate && minDate > maxDate) {
+      return new Error(`Invalid prop \`${propName}\` of type \`${typeof minDate}\` supplied to \`${componentName}\`, minDate cannot be larger than maxDate.`);
+    }
+  }
+
+  // Everything is fine
+  return null;
+};
+
+export const isMaxDate = (props, propName, componentName) => {
+  const { [propName]: maxDate } = props;
+
+  if (maxDate) {
+    if (!(maxDate instanceof Date)) {
+      return new Error(`Invalid prop \`${propName}\` of type \`${typeof maxDate}\` supplied to \`${componentName}\`, expected instance of \`Date\`.`);
+    }
+
+    const { minDate } = props;
+
+    if (minDate && maxDate < minDate) {
+      return new Error(`Invalid prop \`${propName}\` of type \`${typeof maxDate}\` supplied to \`${componentName}\`, maxDate cannot be smaller than minDate.`);
+    }
+  }
+
+  // Everything is fine
+  return null;
+};
+
+export const isValueType = PropTypes.oneOf(allValueTypes);
