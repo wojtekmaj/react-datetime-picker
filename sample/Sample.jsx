668eda3b0d5bb2e2a367df032a6658374353d663
Wojciech Maj <kontakt@wojtekmaj.pl>
2020-11-26 10:44:34 +0100

    Update sample

--- sample/Sample.jsx
+++ sample/Sample.jsx
@@ -1,46 +1,35 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
+import React, { useState } from 'react';
 import DateTimePicker from 'react-datetime-picker';
 
 import './Sample.less';
 
-export default class Sample extends Component {
-  state = {
-    value: new Date(),
-  }
+export default function Sample() {
+  const [value, onChange] = useState(new Date());
 
-  onChange = (value) => this.setState({ value })
-
-  render() {
-    const { value } = this.state;
-
-    return (
-      <div className="Sample">
-        <header>
-          <h1>react-datetime-picker sample page</h1>
-        </header>
-        <div className="Sample__container">
-          <main className="Sample__container__content">
-            <DateTimePicker
-              amPmAriaLabel="Select AM/PM"
-              calendarAriaLabel="Toggle calendar"
-              clearAriaLabel="Clear value"
-              dayAriaLabel="Day"
-              hourAriaLabel="Hour"
-              maxDetail="second"
-              minuteAriaLabel="Minute"
-              monthAriaLabel="Month"
-              nativeInputAriaLabel="Date and time"
-              onChange={this.onChange}
-              secondAriaLabel="Second"
-              value={value}
-              yearAriaLabel="Year"
-            />
-          </main>
-        </div>
+  return (
+    <div className="Sample">
+      <header>
+        <h1>react-datetime-picker sample page</h1>
+      </header>
+      <div className="Sample__container">
+        <main className="Sample__container__content">
+          <DateTimePicker
+            amPmAriaLabel="Select AM/PM"
+            calendarAriaLabel="Toggle calendar"
+            clearAriaLabel="Clear value"
+            dayAriaLabel="Day"
+            hourAriaLabel="Hour"
+            maxDetail="second"
+            minuteAriaLabel="Minute"
+            monthAriaLabel="Month"
+            nativeInputAriaLabel="Date and time"
+            onChange={onChange}
+            secondAriaLabel="Second"
+            value={value}
+            yearAriaLabel="Year"
+          />
+        </main>
       </div>
-    );
-  }
+    </div>
+  );
 }
-
-render(<Sample />, document.getElementById('react-container'));
