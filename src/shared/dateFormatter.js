65ce9a21643e3dcc04394b6a45136a3e384fc421
Wojciech Maj <kontakt@wojtekmaj.pl>
2021-01-06 22:33:25 +0100

    Remove unnecessary eslint-disable

--- src/shared/dateFormatter.js
+++ src/shared/dateFormatter.js
@@ -1,7 +1,5 @@
 import getUserLocale from 'get-user-locale';
 
-/* eslint-disable import/prefer-default-export */
-
 export function getFormatter(options) {
   return (locale, date) => date.toLocaleString(locale || getUserLocale(), options);
 }
