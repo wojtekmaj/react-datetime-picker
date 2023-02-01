1a6aea90d5ec473b2ae62a247e0bcfe262399da2
Wojciech Maj <kontakt@wojtekmaj.pl>
2023-02-01 10:02:47 +0100

    Remove invalid htmlFor on legend element

--- test/ValueOptions.jsx
+++ test/ValueOptions.jsx
@@ -10,8 +10,8 @@ export default function ValueOptions({ setValue, value }) {
   }
 
   return (
-    <fieldset id="valueOptions">
-      <legend htmlFor="valueOptions">Set date and time externally</legend>
+    <fieldset>
+      <legend>Set date and time externally</legend>
 
       <div>
         <label htmlFor="datetime">Date and time</label>
