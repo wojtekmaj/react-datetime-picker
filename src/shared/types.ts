f67c3dd9fc23ddb0aa6730835d6dc1404cb046aa
Wojciech Maj <kontakt@wojtekmaj.pl>
2023-05-08 22:34:53 +0200

    Improve types

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -1,3 +1,5 @@
+type Range<T> = [T, T];
+
 export type AmPmType = 'am' | 'pm';
 
 export type ClassName = string | null | undefined | (string | null | undefined)[];
@@ -6,6 +8,6 @@ export type Detail = 'hour' | 'minute' | 'second';
 
 export type LooseValuePiece = string | Date | null;
 
-export type LooseValue = LooseValuePiece | [LooseValuePiece, LooseValuePiece];
+export type LooseValue = LooseValuePiece | Range<LooseValuePiece>;
 
 export type Value = Date | null;
