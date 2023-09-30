fa13173513252472a3d77f8349766fb0036da81c
Wojciech Maj <kontakt@wojtekmaj.pl>
2023-09-30 20:57:09 +0200

    Use new JSX transform in all sample and test suites

--- test/Test.tsx
+++ test/Test.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import { useRef, useState } from 'react';
 import DateTimePicker from 'react-datetime-picker';
 import 'react-datetime-picker/dist/DateTimePicker.css';
 import 'react-calendar/dist/Calendar.css';
