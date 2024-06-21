b1343ae897dce09c40662e00a61de86e9725070e
Wojciech Maj <kontakt@wojtekmaj.pl>
2024-06-21 10:45:52 +0200

    Fix TypeScript 5.5 compatibility

--- packages/react-datetime-picker/src/DateTimeInput.tsx
+++ packages/react-datetime-picker/src/DateTimeInput.tsx
@@ -502,11 +502,7 @@ export default function DateTimeInput({
     } = {};
     for (const formElement of formElements) {
       values[formElement.name] =
-        formElement.type === 'number'
-          ? 'valueAsNumber' in formElement
-            ? formElement.valueAsNumber
-            : Number(formElement.value)
-          : formElement.value;
+        formElement.type === 'number' ? formElement.valueAsNumber : formElement.value;
     }
 
     const isEveryValueEmpty = formElementsWithoutSelect.every((formElement) => !formElement.value);
