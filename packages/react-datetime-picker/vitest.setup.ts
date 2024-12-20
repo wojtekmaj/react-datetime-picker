55f2d8267ce2d8d72bb48de0f63a5779f10a84d6
Wojciech Maj <kontakt@wojtekmaj.pl>
2024-12-20 16:03:41 +0100

    Add workaround for Intl.NumberFormat mysteriously undefined in unit tests

--- packages/react-datetime-picker/vitest.setup.ts
+++ packages/react-datetime-picker/vitest.setup.ts
@@ -1,7 +1,14 @@
-import { afterEach } from 'vitest';
+import { afterEach, beforeEach } from 'vitest';
 import { cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/vitest';
 import 'vitest-canvas-mock';
+
+// Workaround for a bug in Vitest 3 or happy-dom
+const IntlNumberFormat = Intl.NumberFormat;
+
+beforeEach(() => {
+  Intl.NumberFormat = IntlNumberFormat;
+});
 
 afterEach(() => {
   cleanup();
